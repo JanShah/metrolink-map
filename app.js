56ce3e033a3c92f224a2ee8c9f6a72e38712fbd3
Jan Shah <shahcrew@gmail.com>
2023-10-22 14:47:11 +0100

    working and mapped - new data file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ window.addEventListener('load', start);
  */
 async function getData() {
     if (!localStorage.getItem('data')) {
-        const url = "Metrolink_Stops_Functional.json";
+        const url = "data.json";
         return await fetch(window.location.origin + '/' + url)
             .then(async data => await data.json())
             .then(data => data);
@@ -24,8 +24,7 @@ async function start() {
             featureCollection.addEdge(...conn)
         )
     }
-    featureCollection.addEdge('HPK', 'BKV')
-    featureCollection.addEdge('HPK', 'BKV')
+
     save(featureCollection)
     const drawing = new Canvas(featureCollection, document.getElementById('canvas'))
     drawing.draw()
@@ -33,139 +32,177 @@ async function start() {
 }
 
 class Canvas {
-
     #minLon = 0;
     #maxLon = 0;
     #minLat = 0;
     #maxLat = 0;
     #offset = 10;
-    #sensorRange = 10;
+    #sensorRange = 7;
     #mouseX = 0;
     #mouseY = 0;
-    /**
-     * 
-     * @param {FeatureCollection} featureCollection 
-     * @param {HTMLCanvasElement} canvas 
-     * @param {Number} w 
-     * @param {Number} h 
-     */
-    constructor(featureCollection, canvas, w = window.innerWidth, h = window.innerHeight) {
-        canvas.width = w
-        canvas.height = h
-        this.canvas = canvas
+    #scale = 1;
+    #translateX = 0;
+    #translateY = 0;
+    #dragging = false;
+    #prevMouseX = 0;
+    #prevMouseY = 0;
 
+    constructor(featureCollection, canvas, w = window.innerWidth - 50, h = window.innerHeight - 50) {
+        canvas.width = w;
+        canvas.height = h;
+        this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
-        this.ctx.translate(this.#offset, this.#offset)
-
-        this.featureCollection = featureCollection
-        this.#setBoundaries()
+        this.featureCollection = featureCollection;
+        this.inRange = []
+        this.#setBoundaries();
         this.#addListeners();
-
+        this.draw();
     }
 
     #addListeners() {
-        this.selection = null
-        this.canvas.addEventListener('mousedown', this.#mouseDown.bind(this))
-        this.canvas.addEventListener('mousemove', this.#mouseMove.bind(this))
-        this.canvas.addEventListener('mouseup', this.#mouseUp.bind(this))
+        this.selection = null;
+        this.canvas.addEventListener('mousedown', this.#mouseDown.bind(this));
+        this.canvas.addEventListener('mousemove', this.#mouseMove.bind(this));
+        this.canvas.addEventListener('mouseup', this.#mouseUp.bind(this));
+        this.canvas.addEventListener('wheel', this.#scroll.bind(this));
+    }
+
+    #scroll(e) {
+        const wheel = -Math.sign(e.deltaY) / 10;
+        const zoom = Math.pow(1 + Math.abs(wheel) / 2, wheel > 0 ? 1 : -1);
+        if (this.#scale * zoom >= 1) {
+
+            this.#scale *= zoom;
+
+            // Update the translation values to zoom around the mouse cursor
+            this.#translateX = this.#mouseX - (this.#mouseX - this.#translateX) * zoom;
+            this.#translateY = this.#mouseY - (this.#mouseY - this.#translateY) * zoom;
+            this.draw();
+        }
+
     }
 
     #mouseUp() {
-        if(this.selection && this.inRange.length) {
-            console.log(this.selection.code,this.inRange[0].code)
-            this.featureCollection.addEdge(this.selection.code,this.inRange[0].code)
-            save(this.featureCollection)
+        if (this.selection && this.inRange.length) {
+            this.featureCollection.addEdge(this.selection.code, this.inRange[0].code);
+            save(this.featureCollection);
         }
-        this.selection = null
+        this.selection = null;
+        this.#dragging = false;
     }
 
     #mouseDown(e) {
-        if(this.inRange.length) {
-            this.selection = this.inRange[0]
+        if (this.inRange.length) {
+            this.selection = this.inRange[0];
+        } else {
+            this.#dragging = true;
+            this.#prevMouseX = this.#mouseX;
+            this.#prevMouseY = this.#mouseY;
         }
-        
-
     }
+
     #mouseMove(e) {
-        this.#mouseX = e.offsetX - this.#offset;
-        this.#mouseY = e.offsetY - this.#offset
-       
-        this.inRange = this.featureCollection.features.filter(p=>{
-            const dist = pixelDistance({x:this.#mouseX,y:this.#mouseY},p)
-            if(dist < this.#sensorRange) {
-                p.colour = 'red'
-                return true
+        const transform = this.ctx.getTransform();
+        const unscaledX = (e.offsetX - this.#offset - this.#translateX) / transform.a;
+        const unscaledY = (e.offsetY - this.#offset - this.#translateY) / transform.d;
+        this.#mouseX = unscaledX;
+        this.#mouseY = unscaledY;
+        this.inRange = this.featureCollection.features.filter(p => {
+            const dist = pixelDistance({ x: this.#mouseX, y: this.#mouseY }, p);
+            if (dist < this.#sensorRange) {
+                p.colour = 'red';
+                return true;
             } else {
-                p.colour = 'black'
+                p.colour = 'black';
             }
-        })
-        if(this.selection) {
-            this.selection.colour = 'green'
+        });
+        if (this.selection) {
+            this.selection.colour = 'green';
         }
 
+        if (this.#dragging) {
+            const deltaX = this.#mouseX - this.#prevMouseX;
+            const deltaY = this.#mouseY - this.#prevMouseY;
+
+            // Update the translation values to move the canvas.
+            this.#translateX += deltaX;
+            this.#translateY += deltaY;
+            this.#prevMouseX = this.#mouseX;
+            this.#prevMouseY = this.#mouseY;
+        }
+
+        this.draw();
     }
 
     draw() {
-        const ctx = this.ctx
-        setInterval(() => {
-            ctx.clearRect(-this.#offset, -this.#offset, this.canvas.width + this.#offset, this.canvas.height + this.#offset)
+        const ctx = this.ctx;
 
-            if(this.selection) {
-                this.selection.drawLabel(ctx)
-                ctx.beginPath();
-                ctx.moveTo(this.selection.x,this.selection.y)
-                if(this.inRange.length) {
-                    ctx.lineTo(this.inRange[0].x,this.inRange[0].y)
-                    this.inRange[0].drawLabel(ctx)
-                } else {
-                    ctx.lineTo(this.#mouseX,this.#mouseY)
-                }
-                ctx.stroke()
+        ctx.setTransform(1, 0, 0, 1, 0, 0)
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        ctx.setTransform(this.#scale, 0, 0, this.#scale, this.#translateX, this.#translateY);
+
+        ctx.lineWidth = 3;
+        if (this.selection) {
+            this.selection.drawLabel(ctx);
+            ctx.beginPath();
+            ctx.moveTo(this.selection.x, this.selection.y);
+            if (this.inRange.length && this.inRange[0] !== this.selection) {
+                ctx.strokeStyle = 'green';
+                ctx.lineTo(this.inRange[0].x, this.inRange[0].y);
+            } else {
+                ctx.strokeStyle = 'black';
+                ctx.lineTo(this.#mouseX, this.#mouseY);
             }
-            ctx.beginPath()
-            ctx.arc(this.#mouseX,this.#mouseY,this.#sensorRange,0,Math.PI * 2)
             ctx.stroke();
-            this.featureCollection.draw(ctx)
-        }, 60)
+        }
+        if (this.inRange.length) {
 
+            this.inRange[0].drawLabel(ctx);
+        }
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = 'black';
+        ctx.beginPath();
+        ctx.arc(this.#mouseX, this.#mouseY, this.#sensorRange, 0, Math.PI * 2);
+        ctx.stroke();
+        this.featureCollection.draw(ctx);
     }
 
     setXy(point) {
-        const w = this.canvas.width - 20
-        const h = this.canvas.height - 20
+        const w = this.canvas.width - 20;
+        const h = this.canvas.height - 20;
         const [lon, lat] = point.coordinates;
         const xScale = (w / (this.#maxLon - this.#minLon));
         const yScale = (h / (this.#maxLat - this.#minLat));
-        point.x = (lon - this.#minLon) * xScale;
-        point.y = (this.#maxLat - lat) * yScale; //flipped on the y
-
+        point.x = (lon - this.#minLon) * xScale + 10;
+        point.y = (this.#maxLat - lat) * yScale + 10;
     }
+
     #setBoundaries() {
-        const p = this.featureCollection.points
-        const lonList = p.map(c => c[0])
-        const latList = p.map(c => c[1])
-        this.#minLon = Math.min(...lonList)
-        this.#maxLon = Math.max(...lonList)
-        this.#minLat = Math.min(...latList)
-        this.#maxLat = Math.max(...latList)
+        const p = this.featureCollection.points;
+        const lonList = p.map(c => c[0]);
+        const latList = p.map(c => c[1]);
+        this.#minLon = Math.min(...lonList);
+        this.#maxLon = Math.max(...lonList);
+        this.#minLat = Math.min(...latList);
+        this.#maxLat = Math.max(...latList);
         this.featureCollection.features.forEach(feature =>
             this.setXy(feature.geometry)
-        )
+        );
     }
+
     get boundaries() {
         return {
             minLon: this.#minLon,
             maxLon: this.#maxLon,
             minLat: this.#minLat,
             maxLat: this.#maxLat
-        }
+        };
     }
-
-
 }
 
+
 function save(featureCollection) {
-    debugger
+    // debugger
     featureCollection.connections = featureCollection.edges
     localStorage.setItem('data', JSON.stringify(featureCollection))
 }
@@ -189,10 +226,10 @@ class FeatureCollection {
     }
 
     draw(ctx) {
+        this.#edges.draw(ctx);
         this.features.forEach(feature => {
             feature.draw(ctx)
         });
-        this.#edges.draw(ctx);
     }
 
 
@@ -210,6 +247,7 @@ class FeatureCollection {
         const endStation = this.features.find(feature => feature.code === end);
         if (startStation && endStation) {
             this.#edges.push(startStation, endStation);
+            this.#edges.push(endStation, startStation);
         }
 
     }
@@ -229,7 +267,7 @@ class FeatureCollection {
 
     get pointsXY() {
         return this.features.map(feature => {
-            return {x:feature.x,y:feature.y, feature}
+            return { x: feature.x, y: feature.y, feature }
         });
     }
 
@@ -242,9 +280,9 @@ class EdgeCollection {
 
     push(start, end) {
         const existingEdge = this.edges.filter(edge => {
-            return (edge.start == start && edge.end === end) || (edge.end === start && edge.start === end)
+            return edge.start == start && edge.end === end
         });
-        if (!existingEdge.length)
+        if (!existingEdge.length && start !== end)
             this.edges.push(new Edge(start, end))
     }
 
@@ -280,7 +318,7 @@ class Feature {
     }
 
     drawLabel(ctx) {
-        ctx.fillText(this.code,this.x,this.y)
+        ctx.fillText(this.name, this.x, this.y)
     }
 
     get name() {
@@ -328,7 +366,11 @@ class Point {
         ctx.strokeStyle = this.#stroke
         ctx.beginPath()
         ctx.arc(this.x, this.y, 3, 0, Math.PI * 2)
+        ctx.fillStyle = 'white'
+        ctx.fill()
         ctx.stroke()
+        ctx.fillStyle = 'black'
+
     }
 }
 
